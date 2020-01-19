030bd10ca25a6c0ff62bcdc44265a4627e47619e
Thilakshan <th_thilakshan@hotmail.com>
2020-01-18 22:06:28 -0500

    add authentication and error reducers

---
+++ src/reducers/errorReducer.js
@@ -0,0 +1,28 @@
+import { CLEAR_ERRORS, GET_ERRORS } from '../actions/types'; //Types of actions
+
+const initalState = {
+  message: {},
+  status: null,
+  id: null
+};
+
+export default function(state = initalState, action) {
+  switch (action.type) {
+    case GET_ERRORS:
+      return {
+        msg: action.payload.msg,
+        status: action.payload.status,
+        id: action.payload.id
+      };
+    case CLEAR_ERRORS: {
+      return {
+        message: {},
+        status: null,
+        id: null
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,12 @@
 
 import { combineReducers } from 'redux'; //combines reducers together
 import postReducer from './postReducer.js'; //personal reducer
+import errorReducer from './errorReducer'; //personal reducer
+import authReducer from './authReducer'; //personal reducer
 
 //Combines reducers:
 export default combineReducers({
-  post: postReducer
+  post: postReducer,
+  error: errorReducer,
+  auth: authReducer
 });
