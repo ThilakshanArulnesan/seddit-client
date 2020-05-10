136b3e8318b8e916edadd45276b56116c91f33c3
Thilakshan Arulnesan <thilakshan@brokerbay.ca>
2020-05-09 22:00:13 -0400

    add post styling

--- src/components/Posts/PostListItem.jsx
+++ src/components/Posts/PostListItem.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import styles from './PostListItem.module.scss';
+import moment from 'moment';
 
 const PostListItem = ({ post }) => {
+  const { title, body, date, user } = post;
+  console.log(post);
   return (
     <article className={styles.postContainer}>
-      <h1>Title here</h1>
-      <p>Content here</p>
+      <h1 className={styles.title}>{title}</h1>
+      <div className={styles.postMetaData}>
+        <div className={styles.date}>
+          {moment(date).format('hh:mmA ddd, MMM yyyy')}
+        </div>
+        {user?.name?.firstName && (
+          <div className={styles.submittedBy}>
+            Submitted by: {user.name.firstName}
+          </div>
+        )}
+        <div className={styles.submittedBy}># Comments</div>
+      </div>
+      <p>{body}</p>
     </article>
   );
 };
